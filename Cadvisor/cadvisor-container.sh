5cc91edd1fa5d5f17859f5b90511932f3ec929db
Jean BORT <jean.bort@emvista.com>
2020-06-04 21:26:16 +0200

    format script

--- Cadvisor/cadvisor-container.sh
+++ Cadvisor/cadvisor-container.sh
@@ -1,1 +1,9 @@
-docker run -v /:/rootfs:ro -v /var/run:/var/run:rw -v /sys:/sys:ro -v /var/lib/docker/:/var/lib/docker:ro -p 8181:8080 -d google/cadvisor
+#!/bin/bash
+
+docker run \
+	-v /:/rootfs:ro \
+	-v /var/run:/var/run:rw \
+	-v /sys:/sys:ro \
+	-v /var/lib/docker/:/var/lib/docker:ro \
+	-p 8181:8080 -d \
+	google/cadvisor
